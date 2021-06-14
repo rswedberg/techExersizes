9ea0fc4bb617b89b478b0bb0c73ead5086fed778
mzietlow <>
2021-06-14 16:18:16 +0000

    Riley's personalized code in the branch

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 # Create a flask app
 app = Flask(
@@ -8,9 +8,15 @@ app = Flask(
 )
 
 # Index page
-@app.route('/')
-def hello():
-  return render_template("index.html")
+@app.route('/', methods=["GET", "POST"])
+def index():
+  if request.method == "GET":
+    return render_template("index.html")
+  else:
+    number = int(request.form.get("number"))
+    newNum = number * 2
+    message = "Riley chose the number " + str(number) + " and then it doubles to " + str(newNum)
+    return render_template("index.html", message=message)
 
 if __name__ == '__main__':
   # Run the Flask app
