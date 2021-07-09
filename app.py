b0d5dc0d81ed185a8215240f555ce2c5fa13e7d1
mzietlow <>
2021-07-09 17:01:58 +0000

    New test_basic file for circleci

--- app.py
+++ app.py
@@ -3,13 +3,18 @@ from flask import Flask, render_template, request
 from db import init_db_command
 from db_execute import Value
 import sqlite3
-    
+
+
 # Create a flask app
-app = Flask(
-  __name__,
-  template_folder='templates',
-  static_folder='static'
-)
+def create_app():
+  app = Flask(
+    __name__,
+    template_folder='templates',
+    static_folder='static'
+  )
+  return app
+
+app = create_app()
 
 # Index page
 @app.route('/', methods=["GET", "POST"])
