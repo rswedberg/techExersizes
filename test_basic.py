abfcc3f8d9abc1728b763826ea648144f89779f4
rswedberg <55005538+rswedberg@users.noreply.github.com>
2021-07-23 11:45:13 -0500

    Update test_basic.py for unittest

--- test_basic.py
+++ test_basic.py
@@ -1,24 +1,26 @@
 import unittest
 from flask import current_app
-from app import create_app
+from app import app, create_app
 
 class SampleTest(unittest.TestCase):
   def setUp(self):
-    self.app = create_app('testing')
-    self.app_context = self.app.app_context()
+    self.app = create_app()
+    self.app_context = self.app.test_request_context()
     self.app_context.push()
-
-  def tearDown(self):
-    self.app_context.pop()
+    self.client = self.app.test_client(use_cookies=True)
 
   def test_value(self):
-    response - self.client.post(url_for('index'), data={
+    tester = app.test_client(self)
+    response = tester.post("/", data={
       'number': '5',
     }, follow_redirects=True)
-    self.assertTrue('10' in double.data)
+    self.assertTrue(b'10' in response.data)
 
   def test_app_exists(self):
     self.assertFalse(current_app is None)
 
-  def test_app_is_testing(self):
-    self.assertTrue(current_app.config['TESTING'])
+  def tearDown(self):
+    self.app_context.pop()
+
+if __name__ == "__main__":
+    unittest.main()
